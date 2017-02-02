881f019b7a4fce92db0a7719400851f2d936b6f9
Will Iverson <wiverson@gmail.com>
2017-02-02 12:29:25 -0800

    Version bump. Switched to Assert/J instead of FEST

--- src/test/java/com/dev9/CalculatorTest.java
+++ src/test/java/com/dev9/CalculatorTest.java
@@ -1,11 +1,10 @@
 package com.dev9;
 
-import static org.fest.assertions.api.Assertions.assertThat;
-
-import org.fest.util.VisibleForTesting;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
