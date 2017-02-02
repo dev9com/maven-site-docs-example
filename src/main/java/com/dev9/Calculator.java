b2a1421cf7a52130d5d58eedfad5428b23829a87
Will Iverson <wiverson@gmail.com>
2017-02-02 12:29:42 -0800

    Added javadoc comments

--- src/main/java/com/dev9/Calculator.java
+++ src/main/java/com/dev9/Calculator.java
@@ -3,41 +3,82 @@ package com.dev9;
 import java.util.EventListener;
 import java.util.StringTokenizer;
 
+/**
+ * Simple Calculator for demonstration purposes.
+ */
 public class Calculator {
 
     private double state;
 
+    /**
+     *
+     * @param value to add to register
+     * @return the original Calculator object
+     */
     public Calculator add(double value) {
         state += value;
         return this;
     }
 
+    /**
+     *
+     * @param value to subtract from the register
+     * @return the original Calculator object
+     */
     public Calculator subtract(double value) {
         state -= value;
         return this;
     }
 
+    /**
+     *
+     * @param value to multiply the register by
+     * @return the original Calculator object
+     */
     public Calculator multiply(double value) {
         state = state * value;
         return this;
     }
 
+    /**
+     *
+     * @param value to divide the register by
+     * @return the original Calculator object
+     */
     public Calculator divide(double value) {
         state = state / value;
         return this;
     }
 
+    /**
+     *
+     * @return current value of the register
+     */
     public double value() {
         return state;
     }
 
-    // TODO figure out the transwarp requirements.
-    public double transwarp(double value) {
+    /**
+     *
+     * @param value to apply transwarp value
+     * @return the original Calculator object
+     */
+    public Calculator transwarp(double value) {
+
+        // TODO figure out the transwarp requirements.
         throw new UnsupportedOperationException("No transwarp defined.");
     }
 
     /**
+     *
      * Method intended to trip the PMD violations error. Hopefully you find this method as horrifying as I do even without PMD.
+     *
+     * @param foo random string
+     * @param bar random string
+     * @param spam untyped Object FTW
+     * @param eventListener some kind of events...?
+     * @param stringTokenizer because why not?
+     * @return some number
      */
     public int arbitrarilyComplexMethod(String foo, int bar, Object spam, EventListener eventListener, StringTokenizer stringTokenizer) {
 
@@ -65,6 +106,13 @@ public class Calculator {
 
     /**
      * Method intended to trip the CPM violations error
+     *
+     * @param foo random string
+     * @param bar random string
+     * @param spam untyped Object FTW
+     * @param eventListener some kind of events...?
+     * @param stringTokenizer because why not?
+     * @return some number
      */
     public int arbitrarilyComplexMethodPart2(String foo, int bar, Object spam, EventListener eventListener, StringTokenizer stringTokenizer) {
         try {
